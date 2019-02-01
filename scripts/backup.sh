090f191b51de2b89b81e3ae5ced5664b6ba5523d
Matthew Segal <mattdsegal@gmail.com>
2019-02-01 13:33:40 +1100

    add blog reader

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -3,7 +3,7 @@
 HOST='167.99.78.141'
 ssh root@$HOST /bin/bash << EOF
     set -e
-    for db_name in links war whatson photos
+    for db_name in links war whatson photos reader
     do
         echo "===== backing up \$db_name ====="
        /root/scripts/backup-database.sh \$db_name
