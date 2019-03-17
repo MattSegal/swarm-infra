80d151708ef638b253efcff6f18d2750ad5aadf6
Matthew Segal <mattdsegal@gmail.com>
2019-03-17 16:01:54 +1100

    Fix dockerhost... sortof

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,6 +24,6 @@ ssh root@$HOST /bin/bash << EOF
     docker-compose push
 
     echo "Deploying $REPO to docker swarm"
-    export DOCKERHOST=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)
+    export DOCKERHOST=167.99.78.141
     docker stack deploy --compose-file docker-compose.prod.yml $REPO
 EOF
