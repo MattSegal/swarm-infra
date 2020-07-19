45b27900bd40b59826900456aa1d8359dd4cfecd
Matthew Segal <mattdsegal@gmail.com>
2020-07-19 22:47:12 +1000

    Add NodeJS + Yarn

--- setup/install.sh
+++ setup/install.sh
@@ -31,6 +31,10 @@ add-apt-repository \
    $(lsb_release -cs) \
    stable"
 
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+apt-get install -y nodejs
+npm install -g yarn
+
 echo "Updating apt sources again"
 apt-get update -qq
 
